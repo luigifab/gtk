236a23269421020fcb2f5391f4e5ef0c10160b01
luigifab <31816829+luigifab@users.noreply.github.com>
2025-06-05 16:48:17 +0200

    Update gtkrange.c

--- gtk/gtkrange.c
+++ gtk/gtkrange.c
@@ -3572,6 +3572,8 @@ gtk_range_update_mouse_location (GtkRange *range)
   else
     priv->mouse_location = NULL;
 
+g_print("loc=%s\n", gtk_css_node_get_name (gtk_css_gadget_get_node (priv->mouse_location)));
+
   if (old_location != priv->mouse_location)
     {
       if (old_location != NULL)
