8f27dfbc7521d2593cc73cddd627ac52f32302ae
luigifab <31816829+luigifab@users.noreply.github.com>
2025-06-05 14:23:37 +0200

    Update gtkrange.c

--- gtk/gtkrange.c
+++ gtk/gtkrange.c
@@ -2217,17 +2217,6 @@ gtk_range_size_allocate (GtkWidget     *widget,
   if (gtk_widget_get_realized (widget)) {
     if (GTK_IS_SCROLLBAR (widget)) {
       const gchar *config = g_getenv ("GTK_ENLARGE_SCROLLBARS");
-      //if (config && (strcmp (config, "1") == 0)) {
-      //  if (priv->orientation == GTK_ORIENTATION_VERTICAL)
-      //    gdk_window_move_resize (priv->event_window,
-      //                            allocation->x - 10, allocation->y,
-      //                            allocation->width + 20, allocation->height);
-      //  else
-      //    gdk_window_move_resize (priv->event_window,
-      //                            allocation->x, allocation->y - 10,
-      //                            allocation->width, allocation->height + 20);
-      //}
-      g_print("position = x=%d, y=%d, w=%d, h=%d\n", allocation->x, allocation->y, allocation->width, allocation->height);
       if (config && (strcmp (config, "1") == 0)) {
         GtkWidget *toplevel_widget = gtk_widget_get_toplevel (widget);
         GdkWindow *toplevel_window = gtk_widget_get_window (toplevel_widget);
@@ -2242,10 +2231,9 @@ gtk_range_size_allocate (GtkWidget     *widget,
           gdk_window_reparent (priv->event_window, toplevel_window, rel_x, rel_y - 10);
           gdk_window_resize (priv->event_window, allocation->width, allocation->height + 20);
         }
-        g_print("repositi = x=%d, y=%d, w=%d, h=%d\n", rel_x, rel_y, allocation->width, allocation->height);
       }
       else {
-        gdk_window_move_resize (priv->event_window, // default
+        gdk_window_move_resize (priv->event_window,
                                 allocation->x, allocation->y,
                                 allocation->width, allocation->height);
       }
@@ -2301,9 +2289,6 @@ gtk_range_realize (GtkWidget *widget)
 
   priv->event_window = gdk_window_new (gtk_widget_get_parent_window (widget),
 					&attributes, attributes_mask);
-  GdkRGBA color = { 1.0, 0.0, 0.0, 0.5 }; // lol ?
-  gdk_window_set_background_rgba (priv->event_window, &color);
-  gdk_window_invalidate_rect (priv->event_window, NULL, TRUE);
   gtk_widget_register_window (widget, priv->event_window);
 }
 
